881090be8b1011c46a007c2cfec8a3773b23536f
Ali-Armani <armani.ali@proton.me>
2026-09-10 18:04:21 +0330

    feat: add a paragraph about fruits

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import Header from "./Header.jsx"
 import Food from "./Food.jsx"
+import Desc from "./Desc.jsx"
 import Footer from "./Footer.jsx"
+
 function App() {
   
   return(
     <>
       <Header></Header>
       <Food></Food>
+      <Desc></Desc>
       <Footer></Footer>
     </>
   );
---
+++ src/Desc.jsx
@@ -0,0 +1,11 @@
+
+function Desc() {
+
+  return(
+    <p>
+      Oranges, bananas and apples are fruits. <br /> You should eat enough fruit to stay healthy. <br /> Some fruits, like bananas and apples, taste much better than junk food and sweets! <br /> <br />
+    </p>
+  );
+}
+
+export default Desc
