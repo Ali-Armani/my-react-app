34fdd0d9542210124abfd235dc7c65679ee49c40
Ali-Armani <armani.ali@proton.me>
2026-09-10 19:33:29 +0330

    feat: add a button saying Buy Fruits

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./Header.jsx"
 import Card from "./Card.jsx"
 import Food from "./Food.jsx"
 import Desc from "./Desc.jsx"
+import Button from "./Button.jsx"
 import Footer from "./Footer.jsx"
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Card></Card>
       <Food></Food>
       <Desc></Desc>
+      <Button></Button>
       <Footer></Footer>
     </>
   );
---
+++ src/Button.jsx
@@ -0,0 +1,11 @@
+
+function Button() {
+
+  return(
+    <button className="buy-fruits">
+      Buy Fruits
+    </button>
+  );
+}
+
+export default Button
