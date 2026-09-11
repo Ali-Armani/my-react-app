149f0ffe3982d2d23b092b53e0550ad3b94095a6
Ali-Armani <armani.ali@proton.me>
2026-09-11 17:58:38 +0330

    feat: add 1 more Student tag with null value

--- src/exercises/StudentPage/StudentPage.jsx
+++ src/exercises/StudentPage/StudentPage.jsx
@@ -12,6 +12,8 @@ function StudentPage() {
       <hr />
       <Student name="Marcello" age="18" isStudent={NaN} />
       <hr />
+      <Student isStudent={null} />
+      <hr />
       <Student name="Emily" age="21" isStudent={undefined} />
       <hr />
       <Student name="Luca" age="29" isStudent={false} />
