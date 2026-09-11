fe9e2cf24d6b1db3e12bd361ee43a6f55df071f9
Ali-Armani <armani.ali@proton.me>
2026-09-11 15:17:58 +0330

    fix: correct src attribute value of fruits-photo.webp

--- src/exercises/FoodPage/Card.jsx
+++ src/exercises/FoodPage/Card.jsx
@@ -1,9 +1,11 @@
 
+import fruitsPhoto from '../../assets/fruits-photo.webp';
+
 function Card() {
 
   return (
     <div className="card">
-      <img src="./src/assets/fruits-photo.webp" alt="Fruits picture" className="fruits-photo" />
+      <img src={fruitsPhoto} alt="Fruits picture" className="fruits-photo" />
       <h2>Fruits</h2>
       <p>Apples, oranges and bananas are full of vitamins and are among the heakthiest types of food you can eat.</p>
     </div>
