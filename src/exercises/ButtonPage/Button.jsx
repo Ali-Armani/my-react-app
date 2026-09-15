96d4a4b802b17cfb3a64412dbced021927ee750e
Ali-Armani <armani.ali@proton.me>
2026-09-15 21:29:30 +0330

    feat: add 'if' exercises to ButtonPage

--- src/exercises/ButtonPage/Button.jsx
+++ src/exercises/ButtonPage/Button.jsx
@@ -2,9 +2,9 @@
 function Button() {
 
   return(
-    <>
-    a
-    </>
+    
+      <></>
+    
   )
 }
 
--- src/exercises/ButtonPage/ButtonPage.jsx
+++ src/exercises/ButtonPage/ButtonPage.jsx
@@ -2,11 +2,28 @@
 import Button from './Button';
 
 function ButtonPage() {
+
+  let count = 0;
+
+  const handlelick = () => {
+    if (count < 3) {
+      count++;
+      console.log(`${name} You clicked me ${count} time(s)`);
+    }
+    else {
+      console.log(`${name} That hurt!🤕`)
+    }
+  };
+
+//  const handlelick2 = (name) => console.log(`${name} Stop clicking me!`);
+
   return(
     <>
-      A
+      <button onClick={() => handlelick("Dude!")}>
+        Click me 😀
+      </button>
     </>
   )
 }
 
-export default Button;
+export default ButtonPage;
