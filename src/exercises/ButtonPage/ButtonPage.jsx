7693f1bcc3654d72420e92800cfc6c7b7aea452d
Ali-Armani <armani.ali@proton.me>
2026-09-16 12:57:34 +0330

    feat: update ButtonPage

--- src/exercises/ButtonPage/ButtonPage.jsx
+++ src/exercises/ButtonPage/ButtonPage.jsx
@@ -3,6 +3,7 @@ import Button from './Button';
 
 function ButtonPage() {
 
+/*
   let count = 0;
 
   const handlelick = () => {
@@ -15,11 +16,29 @@ function ButtonPage() {
     }
   };
 
-//  const handlelick2 = (name) => console.log(`${name} Stop clicking me!`);
+  const handlelick2 = (name) => console.log(`${name} Stop clicking me!`);
+*/
+
+
+
+// Event Argument:
+  const handleClick = (e) => e.target.textContent = 'OUCH!🤕';
+
+//  const handleClick = (e) => e.target.style.display = 'none';
+
+/*
+  return(
+    <>
+      <button onDoubleClick={(e) => handleClick(e)}>
+        Click me 😀
+      </button>
+    </>
+  )
+*/
 
   return(
     <>
-      <button onClick={() => handlelick("Dude!")}>
+      <button onClick={(e) => handleClick(e)}>
         Click me 😀
       </button>
     </>
