25102d35b857192d3b14e7f0edaf26c684832ac2
Ali-Armani <armani.ali@proton.me>
2026-09-16 16:13:22 +0330

    feat: add exercise to MyComponent

--- src/exercises/MyComponentPage/MyComponent.jsx
+++ src/exercises/MyComponentPage/MyComponent.jsx
@@ -1,8 +1,64 @@
 
+import React, {useState} from 'react';
+
 function MyComponent() {
 
+  const [name, setName] = useState('Guest');
+
+  const [age, setAge] = useState(0);
+
+  const updateName = () => {
+    setName('SpongeBob');
+  }
+
+  const decrementAge10 = () => {
+    setAge(age - 10);
+  }
+
+  const decrementAge = () => {
+    setAge(age - 1);
+  }
+
+  const resetAge = () => {
+    setAge(0);
+  }
+
+  const incrementAge = () => {
+    setAge(age + 1);
+  }
+
+  const incrementAge10 = () => {
+    setAge(age + 10);
+  }
+
+
+/*
+  const updateAge = () => {
+    setAge('28');
+  }
+
   return(
-    <>A</>
+    <div>
+      <p>Name: {name}</p>
+      <p>Age: {age}</p>
+      <button onClick={updateName}>Set Name</button>
+      <button onClick={updateAge}>Set Age</button>
+    </div>
+  )
+}
+*/
+
+return(
+    <div>
+      <p>Name: {name}</p>
+      <p>Age: {age}</p>
+      <button onClick={updateName}>Set Name</button>
+      <button onClick={decrementAge10}>-10</button>
+      <button onClick={decrementAge}>-</button>
+      <button onClick={resetAge}>Reset</button>
+      <button onClick={incrementAge}>+</button>
+      <button onClick={incrementAge10}>+10</button>
+    </div>
   )
 }
 
