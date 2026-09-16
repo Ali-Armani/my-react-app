1c99f6b51e745f28f53f221d2304b643ec7da057
Ali-Armani <armani.ali@proton.me>
2026-09-16 19:06:19 +0330

    feat: add exercise to CounterPage

--- src/ExerciseSwitcher.jsx
+++ src/ExerciseSwitcher.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import CounterPage from './exercises/CounterPage/Counter'
 import MyComponentPage from './exercises/MyComponentPage/MyComponentPage'
 import ButtonPage from './exercises/ButtonPage/ButtonPage';
 import ListPage2  from './exercises/ListPage2/ListPage2';
@@ -8,6 +9,7 @@ import StudentPage from './exercises/StudentPage/StudentPage';
 import FoodPage from './exercises/FoodPage/FoodPage';
 
 const exercises = {
+  counterPage: CounterPage,
   myComponentPage: MyComponentPage,
   buttonPage: ButtonPage,
   listPage2: ListPage2,
---
+++ src/exercises/CounterPage/CounterPage.jsx
@@ -0,0 +1,13 @@
+
+import Counter from './Counter.jsx'
+
+function CounterPage() {
+
+  return(
+    <>
+      <Counter />
+    </>
+  );
+}
+
+export default CounterPage;
